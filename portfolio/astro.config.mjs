b0cdb844e7f8b8f4adda5a90016659a008ddcc14
Yashodhan Deshmukh <yashodhan.de@gmail.com>
2026-03-20 20:58:30 +0530

    bug-fix

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yashodhan.design',
   output: 'static',
   adapter: vercel(),
+  integrations: [sitemap()],
 });
